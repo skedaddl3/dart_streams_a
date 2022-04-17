ed315b21ce6a77f4e13f5b2669254bd2704c9a9f
skedaddl3 <hexisdylan23@gmail.com>
2022-04-17 13:58:14 +0800

    StreamTransformer deprecated syntax

--- bin/dart_streams_a.dart
+++ bin/dart_streams_a.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
 import 'dart:html';
 
 /*
   This code was not compiled from Dart to JS using webdev build
 */
 void main() {
+  /* Streams Take and Where
   final Element? button = querySelector('button');
   //  final Element? input = querySelector('input'); Old Code
   //  changing type annotation causes error
@@ -24,4 +26,21 @@ void main() {
       // fixed using refactoring at variable declaration line 8
       (event) => print('You got it'),
       onDone: () => print('Nope bad guesses'));
+  */
+  final input = querySelector("input") as InputElement;
+  final div = querySelector("div") as DivElement;
+  final validator =
+      StreamTransformer.fromHandlers(handleData: (inputValue, sink) {
+    if (inputValue.contains('@')) {
+      // another deprecated syntax no fix issued from Q&A
+      sink.add(inputValue);
+    } else {
+      sink.addError('Enter a valid email');
+    }
+  });
+  input.onInput
+      .map((dynamic event) => event.target.value)
+      .transform(validator)
+      .listen(() => print('That email looks valid'),
+          onError: (err) => div.innerHtml = err);
 }
