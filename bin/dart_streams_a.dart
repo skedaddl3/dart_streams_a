ccb16409b04a53a2b3390291e4c2da037ff24ccf
skedaddl3 <hexisdylan23@gmail.com>
2022-04-17 13:08:35 +0800

    error at line 19 input.value with null check

--- bin/dart_streams_a.dart
+++ bin/dart_streams_a.dart
@@ -5,9 +5,19 @@ import 'dart:html';
 */
 void main() {
   final Element? button = querySelector('button');
+  final Element? input = querySelector('input');
+
+  /*
+    Previous Code
 
   button?.onClick
       .timeout(Duration(seconds: 1),
           onTimeout: ((sink) => sink.addError('You lost')))
       .listen((event) {}, onError: (err) => print(err));
+  */
+
+  button!.onClick.take(4).where((event) => input.value == 'banana').listen(
+      // error on .value even with ! null check
+      (event) => print('You got it'),
+      onDone: () => print('Nope bad guesses'));
 }
